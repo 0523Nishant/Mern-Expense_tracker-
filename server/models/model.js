15957c96cf2a1a237991fd731757f8ca3bcc08cc
Nishant <Nishantgupta0523@gmail.com>
2023-05-20 14:21:34 +0530

    first commit

---
+++ server/models/model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+// Category
+const categoriesSchema = mongoose.Schema({
+  type: { type: String, default: "Investment" },
+  color: { type: String, default: "#FCBE44" },
+});
+
+const transactionSchema = mongoose.Schema({
+  name: { type: String, default: "Anonymous" },
+  type: { type: String, default: "Investment" },
+  amount: { type: Number, default: 0 },
+  date: { type: Date, default: Date.now },
+});
+
+var Categories = mongoose.model("Categories", categoriesSchema);
+var Transaction = mongoose.model("Transaction", transactionSchema);
+
+export default { Categories, Transaction };
